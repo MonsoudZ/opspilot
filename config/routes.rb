3118aef73a8f4bee13ac34215e47d89913412c08
Chace Zanaty <chacezanaty@Chaces-MacBook-Air.local>
2025-07-29 17:34:43 -0400

    add devise route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
 
   get "up" => "rails/health#show", as: :rails_health_check
 
@@ -7,7 +6,8 @@ Rails.application.routes.draw do
   authenticate :user do
     mount Sidekiq::Web => '/sidekiq'
   end
-
-  # Defines the root path route ("/")
-  root to: "devise/sessions#new"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
+  
 end
