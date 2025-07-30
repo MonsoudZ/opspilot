10d960e0e6b887357ddac8fc842cf327a9c28c20
Chace Zanaty <chacezanaty@Chaces-MacBook-Air.local>
2025-07-29 21:49:38 -0400

    fixed user test and database url for test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user can be created" do
+    user = User.new(
+      email: 'test@example.com',
+      password: 'password123',
+      password_confirmation: 'password123'
+    )
+    assert user.valid?
+  end
+
+  test "user requires email" do
+    user = User.new(password: 'password123')
+    assert_not user.valid?
+    assert_includes user.errors[:email], "can't be blank"
+  end
+
+  test "user requires password" do
+    user = User.new(email: 'test@example.com')
+    assert_not user.valid?
+    assert_includes user.errors[:password], "can't be blank"
+  end
 end
