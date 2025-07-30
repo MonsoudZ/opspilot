792c82ef80a1a3269ff2f2553ce8c52cdad9522a
Chace Zanaty <chacezanaty@Chaces-MacBook-Air.local>
2025-07-29 21:40:53 -0400

    added linting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  globals: {
+    // Stimulus globals
+    Stimulus: 'readonly',
+    Application: 'readonly',
+    Controller: 'readonly',
+    // Turbo globals
+    Turbo: 'readonly',
+  },
+}; 
