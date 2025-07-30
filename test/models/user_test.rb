2a5ee2a67dd930ee3fb102a390036866e3ac667a
Chace Zanaty <chacezanaty@Chaces-MacBook-Air.local>
2025-07-29 21:51:04 -0400

    linted,fixed user test and database url for test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "user can be created" do
+  test 'user can be created' do
     user = User.new(
       email: 'test@example.com',
       password: 'password123',
@@ -10,13 +10,13 @@ class UserTest < ActiveSupport::TestCase
     assert user.valid?
   end
 
-  test "user requires email" do
+  test 'user requires email' do
     user = User.new(password: 'password123')
     assert_not user.valid?
     assert_includes user.errors[:email], "can't be blank"
   end
 
-  test "user requires password" do
+  test 'user requires password' do
     user = User.new(email: 'test@example.com')
     assert_not user.valid?
     assert_includes user.errors[:password], "can't be blank"
